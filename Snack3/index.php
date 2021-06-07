28674f79d26bb7226fa00e772bece4413a1547e6
NicolaDiSante <nickymacha90@gmail.com>
2021-06-07 09:26:52 +0200

    Completato

---
+++ Snack3/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Snack3</title>
+</head>
+<body>
+
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+foreach($posts as $post => $alue){
+
+  echo "<ul>
+  <li>" . $post . 
+    "<ul>
+      <li>Titolo: " . $value['title'] . "</li>
+      <li>Autore: " . $value['author'] . "</li>
+      <li>Testo: " . $value['text'] . "</li>
+     </ul>
+  </li>
+</ul>";
+};
+?>
+
+
+
+
+  
+</body>
+</html>
